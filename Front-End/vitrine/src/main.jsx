3982362352b97406ae14862f28f1a0d6633288a3
thiago0-08 <housezx210@gmail.com>
2025-06-08 19:58:03 -0300

    criacao do navbar

--- Front-End/vitrine/src/main.jsx
+++ Front-End/vitrine/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import Navbar from '../src/components/navbar.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Navbar />
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
