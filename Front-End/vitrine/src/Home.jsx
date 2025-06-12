a51cf583f7eea24fd1c6b8868fdbfa765e8f0279
thiago0-08 <housezx210@gmail.com>
2025-06-11 21:16:47 -0300

    criacao do recomendados

--- Front-End/vitrine/src/Home.jsx
+++ Front-End/vitrine/src/Home.jsx
@@ -1,13 +1,14 @@
 import Carrossel from './components/Carrossel/carrossel';
 import Cards from './components/cards/cards';
 import Rodape from './components/rodape/rodape';
+import Recomendados from './components/recomendados/recomendados';
 
 const Home = () => {
   return (
     <div>
       
       <Carrossel />
-
+      <Recomendados />
        <Cards />
 
       <Rodape />
