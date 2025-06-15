ae3bdf9dfa745f9d02f6c5b7457dbeebad0f8db4
thiago0-08 <housezx210@gmail.com>
2025-06-15 01:02:36 -0300

    banco no mysql

--- Back-end/Vitrine/obj/Debug/net8.0/Vitrine.AssemblyInfo.cs
+++ Back-end/Vitrine/obj/Debug/net8.0/Vitrine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vitrine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e02ca17a2e9da10c5cdba6aeda5278144c8301f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28be660a780ef25f5668084e33d1569221b56b3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vitrine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vitrine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
